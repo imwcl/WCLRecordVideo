c0fcb39e781bcdcda12773393af13a6f66aa36fc
W_C__L <18842685026@163.com>
2016-05-25 13:46:04 +0800

    add

--- WCLRecordVideo/WCLRecordVideo/WCLRecordEncoder/WCLRecordEncoder.m
+++ WCLRecordVideo/WCLRecordVideo/WCLRecordEncoder/WCLRecordEncoder.m
@@ -44,34 +44,49 @@
         _writer = [AVAssetWriter assetWriterWithURL:url fileType:AVFileTypeMPEG4 error:nil];
         //使其更适合在网络上播放
         _writer.shouldOptimizeForNetworkUse = YES;
-        //录制视频的一些配置，分辨率，编码方式等等
-        NSDictionary* settings = [NSDictionary dictionaryWithObjectsAndKeys:
-                                  AVVideoCodecH264, AVVideoCodecKey,
-                                  [NSNumber numberWithInteger: cx], AVVideoWidthKey,
-                                  [NSNumber numberWithInteger: cy], AVVideoHeightKey,
-                                  nil];
-        //初始化视频写入类
-        _videoInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:settings];
-        //表明输入是否应该调整其处理为实时数据源的数据
-        _videoInput.expectsMediaDataInRealTime = YES;
-        //将视频输入源加入
-        [_writer addInput:_videoInput];
-        
-        //音频的一些配置包括音频各种这里为AAC,音频通道、采样率和音频的比特率
-        settings = [NSDictionary dictionaryWithObjectsAndKeys:
-                    [ NSNumber numberWithInt: kAudioFormatMPEG4AAC], AVFormatIDKey,
-                    [ NSNumber numberWithInt: ch], AVNumberOfChannelsKey,
-                    [ NSNumber numberWithFloat: rate], AVSampleRateKey,
-                    [ NSNumber numberWithInt: 128000], AVEncoderBitRateKey,
-                    nil];
-        //初始化音频写入类
-        _audioInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeAudio outputSettings:settings];
-        //表明输入是否应该调整其处理为实时数据源的数据
-        _audioInput.expectsMediaDataInRealTime = YES;
-        //将音频输入源加入
-        [_writer addInput:_audioInput];
+        //初始化视频输出
+        [self initVideoInputHeight:cy width:cx];
+        //确保采集到rate和ch
+        if (rate != 0 && ch != 0) {
+            //初始化音频输出
+            [self initAudioInputChannels:ch samples:rate];
+        }
     }
     return self;
+}
+
+//初始化视频输入
+- (void)initVideoInputHeight:(NSInteger)cy width:(NSInteger)cx {
+    //录制视频的一些配置，分辨率，编码方式等等
+    NSDictionary* settings = [NSDictionary dictionaryWithObjectsAndKeys:
+                              AVVideoCodecH264, AVVideoCodecKey,
+                              [NSNumber numberWithInteger: cx], AVVideoWidthKey,
+                              [NSNumber numberWithInteger: cy], AVVideoHeightKey,
+                              nil];
+    //初始化视频写入类
+    _videoInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:settings];
+    //表明输入是否应该调整其处理为实时数据源的数据
+    _videoInput.expectsMediaDataInRealTime = YES;
+    //将视频输入源加入
+    [_writer addInput:_videoInput];
+}
+
+//初始化音频输入
+- (void)initAudioInputChannels:(int)ch samples:(Float64)rate {
+    //音频的一些配置包括音频各种这里为AAC,音频通道、采样率和音频的比特率
+    NSDictionary *settings = [NSDictionary dictionaryWithObjectsAndKeys:
+                              [ NSNumber numberWithInt: kAudioFormatMPEG4AAC], AVFormatIDKey,
+                              [ NSNumber numberWithInt: ch], AVNumberOfChannelsKey,
+                              [ NSNumber numberWithFloat: rate], AVSampleRateKey,
+                              [ NSNumber numberWithInt: 128000], AVEncoderBitRateKey,
+                              nil];
+    //初始化音频写入类
+    _audioInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeAudio outputSettings:settings];
+    //表明输入是否应该调整其处理为实时数据源的数据
+    _audioInput.expectsMediaDataInRealTime = YES;
+    //将音频输入源加入
+    [_writer addInput:_audioInput];
+    
 }
 
 //完成视频录制时调用
